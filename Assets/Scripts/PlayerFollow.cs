b4912a33ab8f576fa8a9cd63026322a9cc8d4d71
michaeljanger <michaeljanger@gmail.com>
2018-09-01 20:08:31 -0700

    Rotate camera

--- Assets/Scripts/PlayerFollow.cs
+++ Assets/Scripts/PlayerFollow.cs
@@ -5,32 +5,25 @@ using UnityEngine;
 public class PlayerFollow : MonoBehaviour {
     public GameObject GameCamera;
     public GameObject Player;
-    public Vector3 offset = new Vector3(0f, 5f, 15f);
-    float speed = 10.0f;
 
     public float turnSpeed = 4.0f;
     public Transform player;
 
-    public float height = 1f;
-    public float distance = 10f;
 
-    private Vector3 offsetX;
-    private Vector3 offsetY;
+    public Vector3 offset;
+	public Vector3 offsetZ = new Vector3(0,0,-4f);
 
     // Use this for initialization
     void Start () {
-        offset = new Vector3(0f, 5f, -25f);
-        offsetX = new Vector3(0, height, distance);
-        offsetY = new Vector3(0, 0, distance);
+		offset = new Vector3(player.position.x, player.position.y - 1.0f, player.position.z - 10.0f);
         }
 	
 	// Update is called once per frame
 	void Update () {
-        GameCamera.transform.position = Player.transform.position+offset;
-        offsetX = Quaternion.AngleAxis(Input.GetAxis("Mouse X") * turnSpeed, Vector3.right) * offsetX;
-        offsetY = Quaternion.AngleAxis(Input.GetAxis("Mouse Y") * turnSpeed, Vector3.up) * offsetY;
-       // transform.position = player.position + offsetX;
-        transform.LookAt(player.position);
+       // GameCamera.transform.position = Player.transform.position+offset;
+		offset = Quaternion.AngleAxis (Input.GetAxis("Mouse X") * turnSpeed, Vector3.up) * offset;
+		transform.position = player.position + offset/10 ;
+		transform.LookAt(player.position);
 
         }
   
